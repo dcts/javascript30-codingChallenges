ed16a07fec6b4db63aa5e6b28673e73d5150584e
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-06-18 17:42:41 +0200

    improve button logic

--- assets-shared/js/buttons.js
+++ assets-shared/js/buttons.js
@@ -1,4 +1,16 @@
 // SAY HI
-console.log("JS TRIGGERED: button.js");
+console.log("JS TRIGGERED: buttons.js");
 
-
+// BACK BUTTON
+const bttnBack = document.getElementById("bttn-back");
+bttnBack.onclick = (e) => {
+  console.log("CLICKED!");
+  // SPECIAL CASE FOR HOSTING ON GITHUB PAGES!
+  if (window.origin.includes("github")) {
+    window.location = window.origin + "/javascript30-codingChalleges/";
+  // WHEN RUNNING LOCALLY!
+  } else {
+    window.location = window.origin;
+    console.log("else");
+  }
+};
