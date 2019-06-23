3d99a13bd367140365724229d9ef9643ee48a40e
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-06-23 05:07:55 +0200

    working teclead api fetch

--- days/31/js/cardsLogic.js
+++ days/31/js/cardsLogic.js
@@ -1,12 +1,42 @@
 // say hi
 console.log("TRIGGERED: cardsLogic.js");
 
-// get DOM elements
+// const bttnPower = document.getElementById("bttn-power");
+// const radioPlayer = document.getElementById("radio-player");
+// const title = document.getElementById("title");
 
+// const currentlyPlayingLabel = document.querySelector(".currently-playing-label");
+// const selectedRadio = document.querySelector(".selected-radio");
 
-// ACTION: click on Card
-// 	- if clicked card already expanded -> remove "expanded" 
-//  - else
-// 		close all cards -> remove "expanded" from all cards
-//		expand clicked card -> add "expanded" to target
-//		change footer
+// bttnPower.addEventListener("click", () => {
+//   radioPlayer.classList.toggle("closed");
+//   currentlyPlayingLabel.classList.toggle("transparent");
+//   selectedRadio.classList.toggle("transparent");
+// });
+
+// const closeAllCards = () => {
+//   const cardExtensions = document.querySelectorAll(".card-extend");
+//   cardExtensions.forEach((cardExtension) => {
+//     cardExtension.classList.add("hidden")
+//   });
+// }
+
+// const cards = document.querySelectorAll(".radio-card");
+
+// cards.forEach((card) => {
+//   card.children[0].addEventListener("click", (e) => {
+//     // RADIO IS ALREADY SELECTED (-> close the window)
+//     if (e.currentTarget.innerText.split("\n")[0] === selectedRadio.innerText) {
+//       currentlyPlayingLabel.classList.add("hidden")
+//       selectedRadio.classList.add("hidden")
+//       selectedRadio.innerText = "";
+//     // ANOTHER RADIO OR NO RADIO SELECTED
+//     } else {
+//       currentlyPlayingLabel.classList.remove("hidden")
+//       selectedRadio.classList.remove("hidden")
+//       closeAllCards();
+//       selectedRadio.innerText = card.children[0].children[0].innerText;
+//     }
+//     card.children[1].classList.toggle("hidden");
+//   })
+// });
--- days/31/js/tecleadRadiosApiCall.js
+++ days/31/js/tecleadRadiosApiCall.js
@@ -3,8 +3,83 @@ console.log("TRIGGERED: tecleadRadiosApiCall.js");
 
 // load DOM elements
 
+const injectCard = (radioCard) => {
+  // radioCards
+  console.log(radioCard);
+  const radioCards = document.querySelector(".raido-cards");
+  console.log(radioCards);
+  console.log(typeof(radioCards));
+  radioCards.innerHTML += (radioCard);
+};
+const buildCard = (radio) => {
+  return `
+    <div class="radio-card">
+      <div class="card-top flex justify-space-between">
+        <p class="radio-title card-text">${radio.name}</p>
+        <p class="radio-fq card-text">${radio.frequency}</p>
+      </div>
+      <div class="card-extend flex justify-space-between hidden">
+        <img class="minus" src="img/minus.svg">
+        <img class="radio-avatar" src="${radio.image}">
+        <img class="plus" src="img/plus.svg">
+      </div>
+    </div>
+    <hr>
+  `
+};
+
+const applyListeners = () => {
+  const bttnPower = document.getElementById("bttn-power");
+  const radioPlayer = document.getElementById("radio-player");
+  const title = document.getElementById("title");
+
+  const currentlyPlayingLabel = document.querySelector(".currently-playing-label");
+  const selectedRadio = document.querySelector(".selected-radio");
+
+  bttnPower.addEventListener("click", () => {
+    radioPlayer.classList.toggle("closed");
+    currentlyPlayingLabel.classList.toggle("transparent");
+    selectedRadio.classList.toggle("transparent");
+  });
+
+  const closeAllCards = () => {
+    const cardExtensions = document.querySelectorAll(".card-extend");
+    cardExtensions.forEach((cardExtension) => {
+      cardExtension.classList.add("hidden")
+    });
+  }
+
+  const cards = document.querySelectorAll(".radio-card");
+
+  cards.forEach((card) => {
+    card.children[0].addEventListener("click", (e) => {
+      // RADIO IS ALREADY SELECTED (-> close the window)
+      if (e.currentTarget.innerText.split("\n")[0] === selectedRadio.innerText) {
+        currentlyPlayingLabel.classList.add("hidden")
+        selectedRadio.classList.add("hidden")
+        selectedRadio.innerText = "";
+      // ANOTHER RADIO OR NO RADIO SELECTED
+      } else {
+        currentlyPlayingLabel.classList.remove("hidden")
+        selectedRadio.classList.remove("hidden")
+        closeAllCards();
+        selectedRadio.innerText = card.children[0].children[0].innerText;
+      }
+      card.children[1].classList.toggle("hidden");
+    })
+  });
+}
 
 // fetch Data
+fetch("https://teclead.de/recruiting/radios")
+.then(resp => resp.json())
+.then(data => {
+  const radioCards = [];
+  data.radios.forEach((radio) => {
+    injectCard(buildCard(radio));
+  });
+  applyListeners();
+})
 
 
 
